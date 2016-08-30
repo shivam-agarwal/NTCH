3475f8c07d7eb379688a23e17fd0d7cdd69d0dff
Shivam <shivamandwork@gmail.com>
2016-08-30 13:27:03 +0530

    requirement

--- server.py
+++ server.py
@@ -831,9 +831,9 @@ def similarity():
 # 	a = {"message":"success", "matrix": returnMatrix}
 # 	return json.dumps(a)
 
-
+port = os.getenv('VCAP_APP_PORT', '5000')
 		
 if __name__ == '__main__':
-    app.debug = False
-    app.run()
+    # app.debug = False
+    app.run(host='0.0.0.0', port=int(port), debug=True)
     conn.close()
